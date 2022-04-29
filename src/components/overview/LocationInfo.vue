e47e194d5c2a4e42dc6c5a6e513023a71172716b
wwp <wwp.personal@outlook.com>
2022-04-29 18:57:55 +0800

    perf: 添加机箱状态

--- src/components/overview/LocationInfo.vue
+++ src/components/overview/LocationInfo.vue
@@ -54,11 +54,17 @@
       <span>{{ smokeSensorPoint?.pointLocation }}</span>
       <img :src="smokeSensorPoint.noSmoke ? smokeNormal : smokeWarning" alt="" />
     </div>
+    <div class="crate-1-wrap"><img src="@/assets/images/overview/crate-1-blue.png" alt="" /></div>
+    <div class="crate-2-wrap"><img :src="iot" alt="" /></div>
+    <div class="crate-3-wrap"><img :src="dataPlatform1" alt="" /></div>
+    <div class="crate-4-wrap"><img :src="dataPlatform2" alt="" /></div>
+    <div class="crate-5-wrap"><img :src="devOps" alt="" /></div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
+import { ref, computed, onMounted } from 'vue'
+import { apiGetSystemDeploy } from '@/service/api/overview'
 import waterNormal from '@/assets/images/overview/water-location-normal.png'
 import waterWarning from '@/assets/images/overview/water-location-warning.png'
 import sensorNormal from '@/assets/images/overview/sensor-normal.png'
@@ -67,6 +73,22 @@ import doorNormal from '@/assets/images/overview/door-normal.png'
 import doorWarning from '@/assets/images/overview/door-warning.png'
 import smokeNormal from '@/assets/images/overview/smoke-normal.png'
 import smokeWarning from '@/assets/images/overview/smoke-warning.png'
+
+import crate2Blue from '@/assets/images/overview/crate-2-blue.png'
+import crate2Yellow from '@/assets/images/overview/crate-2-yellow.png'
+import crate2Red from '@/assets/images/overview/crate-2-red.png'
+
+import crate3Blue from '@/assets/images/overview/crate-3-blue.png'
+import crate3Yellow from '@/assets/images/overview/crate-3-yellow.png'
+import crate3Red from '@/assets/images/overview/crate-3-red.png'
+
+import crate4Blue from '@/assets/images/overview/crate-4-blue.png'
+import crate4Yellow from '@/assets/images/overview/crate-4-yellow.png'
+import crate4Red from '@/assets/images/overview/crate-4-red.png'
+
+import crate5Blue from '@/assets/images/overview/crate-5-blue.png'
+import crate5Yellow from '@/assets/images/overview/crate-5-yellow.png'
+import crate5Red from '@/assets/images/overview/crate-5-red.png'
 
 const props = defineProps<{
   overviewData: any[]
@@ -92,6 +114,93 @@ const doorMagneticPoint = computed(() => props.doorMagnetic.filter((item: any) =
 
 // 烟雾传感器
 const smokeSensorPoint = computed(() => props.smokeSensor.filter((item: any) => item.pointCode === '1')[0] || {})
+
+// 物管平台&&配网监测
+const iot = computed(() => {
+  const result = tableData.value.filter(
+    (item: any) => item.systemCode === 'iot' || item.systemCode === 'distribution_network',
+  )
+  if (result.length > 0) {
+    if (result[0]?.systemStatus === 'red' || result[1]?.systemStatus === 'red') {
+      return crate2Red
+    } else if (result[0]?.systemStatus === 'yellow' || result[1]?.systemStatus === 'yellow') {
+      return crate2Yellow
+    } else {
+      return crate2Blue
+    }
+  } else {
+    return crate2Blue
+  }
+})
+
+// 数据中台
+const dataPlatform1 = computed(() => {
+  const result = tableData.value.filter((item: any) => item.systemCode === 'data_platform')
+  if (result.length > 0) {
+    switch (result[0].systemStatus) {
+      case 'red':
+        return crate3Red
+      case 'yellow':
+        return crate3Yellow
+      case 'green':
+        return crate3Blue
+      default:
+        return crate3Blue
+    }
+  } else {
+    return crate3Blue
+  }
+})
+
+// 数据中台
+const dataPlatform2 = computed(() => {
+  const result = tableData.value.filter((item: any) => item.systemCode === 'data_platform')
+  if (result.length > 0) {
+    switch (result[0].systemStatus) {
+      case 'red':
+        return crate4Red
+      case 'yellow':
+        return crate4Yellow
+      case 'green':
+        return crate4Blue
+      default:
+        return crate4Blue
+    }
+  } else {
+    return crate4Blue
+  }
+})
+
+// 指挥中心&&运维监控
+const devOps = computed(() => {
+  const result = tableData.value.filter(
+    (item: any) => item.systemCode === 'command_center' || item.systemCode === 'cvit_monitor',
+  )
+  if (result.length > 0) {
+    if (result[0]?.systemStatus === 'red' || result[1]?.systemStatus === 'red') {
+      return crate5Red
+    } else if (result[0]?.systemStatus === 'yellow' || result[1]?.systemStatus === 'yellow') {
+      return crate5Yellow
+    } else {
+      return crate5Blue
+    }
+  } else {
+    return crate5Blue
+  }
+})
+
+const tableData = ref<any[]>([])
+
+onMounted(() => {
+  getSystemDeploy()
+})
+
+const getSystemDeploy = async () => {
+  const { code, data } = await apiGetSystemDeploy()
+  if (code === 20000) {
+    tableData.value = data
+  }
+}
 
 /**
  * @desc 判断点位监测是否正常
@@ -153,6 +262,60 @@ const isNormal = (temperature: number, humidity: number) => {
     }
   }
 
+  .crate-1-wrap {
+    position: absolute;
+    top: 619px;
+    right: 673px;
+    z-index: 102;
+
+    img {
+      width: 95px;
+    }
+  }
+
+  .crate-2-wrap {
+    position: absolute;
+    top: 547px;
+    right: 708px;
+    z-index: 101;
+
+    img {
+      width: 157px;
+    }
+  }
+
+  .crate-3-wrap {
+    position: absolute;
+    top: 525px;
+    left: 987px;
+    z-index: 100;
+
+    img {
+      width: 155px;
+    }
+  }
+
+  .crate-4-wrap {
+    position: absolute;
+    top: 506px;
+    left: 921px;
+    z-index: 99;
+
+    img {
+      width: 155px;
+    }
+  }
+
+  .crate-5-wrap {
+    position: absolute;
+    top: 485px;
+    left: 855px;
+
+    img {
+      width: 155px;
+    }
+  }
+
   .water-position-one {
     position: absolute;
     top: 8%;
@@ -183,6 +346,7 @@ const isNormal = (temperature: number, humidity: number) => {
   .sensor-position-three {
     top: 47.5%;
     left: 42.8%;
+    z-index: 105;
     .sensor-position;
   }
 
